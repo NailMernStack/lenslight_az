b7bab8e54b3442888dd5de1d41a5dd2b12dd7288
Nail <memmedovnail27@gmail.com>
2023-08-06 10:11:27 +0400

    Upload

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express  from "express";
+import dotenv from 'dotenv'
+import ejs from "ejs"
+import bodyParser from "body-parser";
+import conn from './db.js'
+import pageRoute from './routes/pageRoute.js'
 
+dotenv.config();
+
+//connection to the DB
+conn();
 
 const app = express();
 
-app.get("/",function(req,res){
-    res.send("Index sayfasi")
-})
+app.use(express.static('public'));
+app.set("view engine","ejs");
 
+// route
+app.use('/', pageRoute);
 
-
+ 
 app.listen(3000,function(){
     console.log("Server aktivdir.")
 })
