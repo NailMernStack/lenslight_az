717ecd2a9eb0a6f7ead5f504b983086cf1572688
NailMernStack <117720592+NailMernStack@users.noreply.github.com>
2023-08-16 21:29:44 +0400

    9kjj

--- Lesson.js
+++ Lesson.js
@@ -1,1 +1,17 @@
 
+let proverb = [
+  "Damlaya damlaya göl olur.",
+  "Ağaç yaşken eğilir.",
+  "Akıllı sır saklar, ahmak pişman olur.",
+  "Her yiğidin bir yoğurt yiyişi vardır.",
+  "Bir elin nesi var, iki elin sesi var.",
+  "Yavaş yürüyüp uzak gitmek, hiç yürümekten iyidir.",
+  "Taşıma suyla değirmen dönmez.",
+  "Göz var nizam var.",
+  "Yiğit otuz yaşında belli olur.",
+  "El elin eşeğini türkü çağırarak arar."
+]
+function RandomProverb(){
+    let randomNumber = Math.random() * proverb.length + 1
+    alert(`${proverb[randomNumber]}`)
+}
